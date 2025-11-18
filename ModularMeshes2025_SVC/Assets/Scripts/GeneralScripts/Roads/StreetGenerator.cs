ff6fd898e89f25939a378c49d4b169aeb6cf9bcb
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2025-11-18 12:01:13 +0100

    Random fix

--- ModularMeshes2025_SVC/Assets/Scripts/GeneralScripts/Roads/StreetGenerator.cs
+++ ModularMeshes2025_SVC/Assets/Scripts/GeneralScripts/Roads/StreetGenerator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class CityRoadGenerator : MonoBehaviour
 {
